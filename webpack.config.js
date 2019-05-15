bc54e88a275d8d47ff70e7ec3abde36b29804857
Anthony <736252868@qq.com>
2019-05-15 22:21:48 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
+/*
+* @Author: Anthony_One
+* @Date:   2019-05-15 21:55:01
+* @Last Modified by:   Anthony_One
+* @Last Modified time: 2019-05-15 22:20:01
+*/
 var path = require("path");
 
-module.exports = {
-    entry: './src/page/index/app.js',
+var config = {
+    entry: {
+    	'index': ['./src/page/index/app.js'],
+    	'cats': ['./src/page/index/cats.js']
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'app.js'
+        filename: 'js/[name].js'
     }
 }
+
+module.exports = config;
