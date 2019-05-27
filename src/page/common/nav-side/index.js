a6a40c7fba48dd103970e9f9f8b9e0a8f42f0cb4
Anthony <736252868@qq.com>
2019-05-27 22:00:50 +0800

    添加nav-side

---
+++ src/page/common/nav-side/index.js
@@ -0,0 +1,39 @@
+
+require('./index.css');
+var _mall           = require('util/mall.js');
+var templateIndex   = require('./index.string');
+// 侧边导航
+var navSide = {
+    //选项设置
+    option : {
+        name : '',
+        navList : [
+            {name : 'user-center', desc : '个人中心', href: './user-center.html'},
+            {name : 'order-list', desc : '我的订单', href: './order-list.html'},
+            {name : 'user-pass-update', desc : '修改密码', href: './user-pass-update.html'},
+            {name : 'about', desc : '关于Xbook-mall', href: './about.html'}
+        ]
+    },
+    init : function(option){
+        // 合并选项
+        $.extend(this.option, option);
+        this.renderNav();
+    },
+    // 渲染导航菜单
+    renderNav : function(){
+        // 计算active数据
+        for(var i = 0, iLength = this.option.navList.length; i < iLength; i++){
+            if(this.option.navList[i].name === this.option.name){
+                this.option.navList[i].isActive = true;
+            }
+        }
+        // 通过模版渲染html
+        var navHtml = _mall.renderHtml(templateIndex, {
+            navList : this.option.navList
+        });
+        // 把html放入容器
+        $('.nav-side').html(navHtml);
+    }
+};
+
+module.exports = navSide;
--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,7 +1,13 @@
 
 console.log("i love you")
 require('./index.css');
-require('page/common/header/index')
+require('page/common/header/index');
+require('page/common/nav/index');
+var navSide = require('page/common/nav-side/index');
+navSide.init({
+    name : 'user-center' //选择哪个子项
+});
+
 var _mall = require('util/mall');
 _mall.request({
     url : 'http://test.happymmall.com/product/list.do?keyword=p',
