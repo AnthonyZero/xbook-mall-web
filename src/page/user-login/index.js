7e6e15abc19973db855871804bfc84888b884696
Anthony <736252868@qq.com>
2019-05-28 00:44:43 +0800

    用户登录

--- src/page/user-login/index.js
+++ src/page/user-login/index.js
@@ -4,3 +4,57 @@ require('./index.css');
 require('page/common/nav-simple/index');
 
 var _mall = require('util/mall');
+var _user   = require('service/user-service.js');
+// 表单里的错误提示
+var formError = {
+    show : function(errMsg){
+        $('.err-item').show().find('.err-msg').text(errMsg);
+    },
+    hide : function(){
+        $('.err-item').hide().find('.err-msg').text('');
+    }
+};
+// page 逻辑部分
+var page = {
+    // 初始化页面
+    init : function(){
+        this.bindEvent();
+    },
+    // 绑定事件
+    bindEvent : function(){
+        var _this = this;
+        $('#submit').click(function(){
+            _this.submit();
+        });
+        //回车事件 也提交
+        $('.user-content').keyup(function(e){
+            if(e.keyCode === 13){
+                _this.submit();
+            }
+        });
+    },
+    // 提交按钮事件
+    submit : function(){
+        var _this = this,
+            formData = {
+                username : $.trim($('#username').val()),
+                password : $.trim($('#password').val())
+            };
+        if(!_mall.validate(formData.username, 'require')){
+            formError.show("用户名不能为空!");
+        }
+        else if(!_mall.validate(formData.password, 'require')){
+            formError.show("密码不能为空!");
+        }
+        else{
+            _user.login(formData, function(res){
+                window.location.href ='./index.html';
+            }, function(errMsg){
+                formError.show(errMsg);
+            });
+        }
+    }
+};
+$(function(){
+    page.init();
+});
---
+++ src/service/user-service.js
@@ -0,0 +1,120 @@
+/**
+ * 用户模块
+ */
+var _mall = require('util/mall.js');
+
+var _user = {
+    // 用户登录
+    login : function(userInfo, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/login.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 检查用户名
+    checkUsername : function(username, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/check_valid.do'),
+            data    : {
+                type    : 'username',
+                str     : username
+            },
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 用户注册
+    register : function(userInfo, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/register.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 检查登录状态
+    checkLogin : function(resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/get_user_info.do'),
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取用户密码提示问题
+    getQuestion : function(username, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/forget_get_question.do'),
+            data    : {
+                username : username
+            },
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 检查密码提示问题答案
+    checkAnswer : function(userInfo, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/forget_check_answer.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 重置密码
+    resetPassword : function(userInfo, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/forget_reset_password.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取用户信息
+    getUserInfo : function(resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/get_information.do'),
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 更新个人信息
+    updateUserInfo : function(userInfo, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/update_information.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 登录状态下更新密码
+    updatePassword : function(userInfo, resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/reset_password.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 登出
+    logout : function(resolve, reject){
+        _mall.request({
+            url     : _mall.getServerUrl('/user/logout.do'),
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    }
+};
+module.exports = _user;
