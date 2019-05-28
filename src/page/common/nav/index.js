8092cb8fe68f46975fcfa7a73bccbe79c6701e34
Anthony <736252868@qq.com>
2019-05-28 22:03:44 +0800

    首页填充

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -1,2 +1,32 @@
 
 require('./index.css');
+var _mall     = require('util/mall.js');
+// 导航
+var nav = {
+    //初始化
+    init : function(){
+        this.bindEvent();
+        return this;
+    },
+    //绑定事件
+    bindEvent : function(){
+        // 登录点击事件
+        $('.js-login').click(function(){
+            _mall.doLogin();
+        });
+        // 注册点击事件
+        $('.js-register').click(function(){
+            window.location.href = './user-register.html';
+        });
+        // 退出点击事件
+        $('.js-logout').click(function(){
+            _user.logout(function(res){
+                window.location.reload();
+            }, function(errMsg){
+                _mall.errorTips(errMsg);
+            });
+        });
+    },
+};
+
+module.exports = nav.init();
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,5 +1,4 @@
 
-console.log("i love you")
 require('./index.css');
 require('page/common/header/index');
 require('page/common/nav/index');
@@ -9,13 +8,3 @@ navSide.init({
 });
 
 var _mall = require('util/mall');
-_mall.request({
-    url : 'http://test.happymmall.com/product/list.do?keyword=p',
-    success: function (res, msg) {
-        console.log("成功")
-        console.log(res, msg);
-    },
-    error: function (msg) {
-        console.log(msg);
-    }
-})
