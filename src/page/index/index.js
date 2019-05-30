43c1539f50bad2cef2fa24cdcdd01baf6edd8df6
Anthony <736252868@qq.com>
2019-05-30 22:24:14 +0800

    index

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,10 +1,5 @@
 
 require('./index.css');
-require('page/common/header/index');
-require('page/common/nav/index');
-var navSide = require('page/common/nav-side/index');
-navSide.init({
-    name : 'user-center' //选择哪个子项
-});
-
-var _mall = require('util/mall');
+var _mall = require('util/mall.js');
+require('page/common/nav/index.js');
+require('page/common/header/index.js');
