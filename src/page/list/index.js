07d83a4a17945dd295c54de3336966d62c8f9a97
Anthony <736252868@qq.com>
2019-06-02 22:54:25 +0800

    商品列表

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -5,6 +5,7 @@ require('page/common/header/index.js');
 var _mall           = require('util/mall.js');
 var _product        = require('service/product-service.js');
 var templateIndex   = require('./index.string');
+var Pagination      = require('util/pagination/index.js');
 var page = {
     //初始化数据
     data : {
@@ -36,10 +37,38 @@ var page = {
             listParam   = this.data.listParam,
             $pListCon   = $('.p-list-con');
         $pListCon.html('<div class="loading"></div>');
+        // 删除参数中不必要的字段
+        listParam.categoryId
+            ? (delete listParam.keyword) : (delete listParam.categoryId);
+        // 请求接口
+        _product.getProductList(listParam, function(res){
+            listHtml = _mall.renderHtml(templateIndex, {
+                list :  res.list
+            });
+            $pListCon.html(listHtml);
+            _this.loadPagination({
+                hasPreviousPage : res.hasPreviousPage,
+                prePage         : res.prePage,
+                hasNextPage     : res.hasNextPage,
+                nextPage        : res.nextPage,
+                pageNum         : res.pageNum,
+                pages           : res.pages
+            });
+        }, function(errMsg){
+            _mall.errorTips(errMsg);
+        });
     },
     // 加载分页信息
     loadPagination : function(pageInfo){
-
+        var _this = this;
+        this.pagination ? '' : (this.pagination = new Pagination());
+        this.pagination.render($.extend({}, pageInfo, {
+            container : $('.pagination'),
+            onSelectPage : function(pageNum){
+                _this.data.listParam.pageNum = pageNum;
+                _this.loadList();
+            }
+        }));
     }
 };
 
